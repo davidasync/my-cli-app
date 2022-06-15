f3826926aed2be7dee5aab23ca944937f6e30449
david.d <david.d@linecorp.com>
2022-06-15 15:07:08 +0700

    Add validation before execute search funtion

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const START = process.argv[2];
 const LIMIT = process.argv[3];
 const SORT = process.argv[4];
 
-const hotelDiscovery = new HotelDiscovery(Number(START), Number(LIMIT), SORT);
-console.log(hotelDiscovery.getHotels())
+if (!START || !LIMIT) {
+  console.error('START & LIMIT are required')
+} {
+  const hotelDiscovery = new HotelDiscovery(Number(START), Number(LIMIT), SORT);
+  console.log(hotelDiscovery.getHotels())
+}
